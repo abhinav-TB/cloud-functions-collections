032d844c5172a7d00b27e6a7b36eda1148172e2b
abhinav-TB <abhinavtb@gmail.com>
2020-04-02 10:01:31 +0530

    authentication triggers tested

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,13 @@
 const functions = require('firebase-functions');
 
+// auth trigger
+
+exports.newUserSighUp=functions.auth.user().onCreate(user=>{
+    console.log("user created,user.email,user.uid")
+});
+
+exports.userDeleted=functions.auth.user().onDelete(user=>{
+    console.log("user deleted",user.email,user.uid)
+});
+
+
